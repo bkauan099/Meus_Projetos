05e57691855e92cc1a3d7bd241c60619b3e03983
bkauan099 <bkauan099@gmail.com>
2025-02-03 15:09:52 -0300

    update-;

--- Desafios Trilha/Desafio 1 - Backend/Desafio_1.js
+++ Desafios Trilha/Desafio 1 - Backend/Desafio_1.js
@@ -1,116 +1,116 @@
 // 1º):
-let nome = "Bruno"
-console.log(nome)
+let nome = "Bruno";
+console.log(nome);
 
 // 2º):
-let idade = 25
-let altura = 1.75
+let idade = 25;
+let altura = 1.75;
 console.log(idade, altura);
 
 // 3º):
-let preco = 50
-let desconto = 0.2
-let precoDesconto = preco - (preco * desconto)
+let preco = 50;
+let desconto = 0.2;
+let precoDesconto = preco - (preco * desconto);
 
-console.log(precoDesconto)
+console.log(precoDesconto);
 
 // 4º):
-let temperatura = 30
+let temperatura = 30;
 
 if (temperatura > 25){
-    console.log("Está calor!")
+    console.log("Está calor!");
 }
 else{
-    console.log("Está fresco!")
+    console.log("Está fresco!");
 }
 
 // 5º):
-let idade = 10 
+let idade = 10;
 if (idade >= 18){
-    console.log("Você é maior de idade!")
+    console.log("Você é maior de idade!");
 }
 else{
-    console.log("Você é menor de idade!")
+    console.log("Você é menor de idade!");
 }
 
 // 6º):
-let nota = 7
+let nota = 7;
 
 if (nota >= 7){
-    console.log("Aprovado")
+    console.log("Aprovado");
 }
 else{
     if (nota >= 5 && nota <= 6){
-        console.log("Recuperação") 
+        console.log("Recuperação");
     }
     else{
-        console.log("Reprovado")
+        console.log("Reprovado");
     }
 }
 
 // 7º):
-let numero1 = 5
-let numero2 = 6
+let numero1 = 5;
+let numero2 = 6;
 if (numero1 == numero2){
-    console.log("Os números são iguais")
+    console.log("Os números são iguais");
 }
 else{
-    console.log("Os números são diferentes")
+    console.log("Os números são diferentes");
 }
 
 // 8º):
-let nome = "Bruno"
-let idade = 20
+let nome = "Bruno";
+let idade = 20;
 
-console.log(`Olá, meu nome é ${nome} e eu tenho ${idade} anos`)
+console.log(`Olá, meu nome é ${nome} e eu tenho ${idade} anos`);
 
 // 9º):
 for (let i = 1; i <= 10; i++){
-    console.log(i)
+    console.log(i);
 }
 
 // 10º):
-let numero = prompt("Digite um número: ")
+let numero = prompt("Digite um número: ");
 
 while (numero != 5){
-    numero = prompt("Digite um número:")
+    numero = prompt("Digite um número:");
 }
 
 // 11º):
 for (let i = 1; i <= 10; i++){
-    console.log(`${i} x 7 = ${7 * i}`)
+    console.log(`${i} x 7 = ${7 * i}`);
 }
 
 // 12º):
 for (let i = 1; i <= 20; i++){
     if (i % 2 == 0){
-        console.log(i)
+        console.log(i);
     }
 }
 
 // 13º):
 function calcularAreaCirculo(raio){
-    let area = 3.14 * raio**2
-    return console.log(area)
+    let area = 3.14 * raio**2;
+    return console.log(area);
 }
 
-calcularAreaCirculo(4)
+calcularAreaCirculo(4);
 
 // 14º):
 // Definição dos valores de cada variável
-let numero1 = 32
-let numero2 = 21
+let numero1 = 32;
+let numero2 = 21;
 
 // Definição de uma variável que vai receber a soma dos dois números
-let soma = numero1 + numero2
+let soma = numero1 + numero2;
 
 // impressão do resultado
-console.log(soma)
+console.log(soma);
 
 // 15º):
 function somarNumeros(numero1, numero2){
-    let soma = numero1 + numero2
-    return console.log(soma)
+    let soma = numero1 + numero2;
+    return console.log(soma);
 }
 
-somarNumeros(2, 5)
+somarNumeros(2, 5);
